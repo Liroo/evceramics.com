f3ccf813972abc8df31a372edda0103ca0f430ef
Liroo <liroo.pierre@gmail.com>
2024-06-11 15:38:30 +0200

    Remove console.log

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -57,8 +57,6 @@ function MainMenu({ menu }: Props) {
   let pathname = usePathname();
   const locale = useLocale();
 
-  console.log(pathname, menu);
-
   return (
     <div className="flex select-none flex-col gap-[6px] text-clay-dark laptop:flex-row laptop:gap-0">
       {menu.map((item, index) => (
