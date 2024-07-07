18207254ac1e14e89aeee904b3eca9f3eee46a93
Liroo <liroo.pierre@gmail.com>
2024-07-07 14:49:54 +0200

    add metadata informations for SEO

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -7,9 +7,6 @@ import { Suspense } from 'react';
 export const metadata = {
   description:
     'EV Ceramics is a ceramic studio run by Emilie Vizcano. All the pieces she makes are handmade, one by one, using the modelling technique.',
-  openGraph: {
-    type: 'website',
-  },
 };
 
 export default async function HomePage({
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,33 @@ export const metadata = {
   description:
     'EV Ceramics is a ceramic studio run by Emilie Vizcano. All the pieces she makes are handmade, one by one, using the modelling technique.',
   openGraph: {
+    url: 'https://www.evceramics.com/',
+    siteName: 'Ev Ceramics',
+    images: [
+      {
+        url: 'https://www.evceramics.com/images/infos/emilie.jpg',
+      },
+    ],
     type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ev Ceramics',
+    creator: '@sen_vz',
+    images: ['https://www.evceramics.com/images/infos/emilie.jpg'], // Must be an absolute URL
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
   },
 };
 
