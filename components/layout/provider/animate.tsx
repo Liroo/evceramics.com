767770a200bbdf44832db06908b6b444ba44fddf
Liroo <liroo.pierre@gmail.com>
2024-09-18 13:45:11 +0200

    Remove animation for now

--- components/layout/provider/animate.tsx
+++ components/layout/provider/animate.tsx
@@ -2,7 +2,6 @@
 
 import { PropsWithChildren, useContext, useRef } from 'react';
 
-import { AnimatePresence, motion } from 'framer-motion';
 import { usePathname } from 'lib/navigation';
 import { LayoutRouterContext } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
@@ -22,17 +21,20 @@ export default function LayoutProviderAnimate({ children }: LayoutProviderAnimat
   const pathname = usePathname();
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={pathname}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.2, ease: 'easeInOut' }}
-        className="h-full w-full"
-      >
-        <FrozenRouter>{children}</FrozenRouter>
-      </motion.div>
-    </AnimatePresence>
+    // <AnimatePresence mode="wait">
+    //   <motion.div
+    //     key={pathname}
+    //     initial={{ opacity: 0 }}
+    //     animate={{ opacity: 1 }}
+    //     exit={{ opacity: 0 }}
+    //     transition={{ duration: 0.2, ease: 'easeInOut' }}
+    //     className="h-full w-full"
+    //   >
+    //     <FrozenRouter>
+    //       {children}
+    //       </FrozenRouter>
+    //   </motion.div>
+    // </AnimatePresence>
+    <>{children}</>
   );
 }
